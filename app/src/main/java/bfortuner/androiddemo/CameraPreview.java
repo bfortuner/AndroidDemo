c90a29b5d2cc0345defb3394cc4517cf43520b85
Brendan Fortuner <bfortuner@gmail.com>
2017-11-25 17:20:19 -0800

    added camera preview

--- app/src/main/java/bfortuner/androiddemo/CameraPreview.java
+++ app/src/main/java/bfortuner/androiddemo/CameraPreview.java
@@ -1,18 +1,19 @@
 package bfortuner.androiddemo;
 
-import android.content.pm.ActivityInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class CameraPreview extends AppCompatActivity {
-
-    //https://androidze.wordpress.com/
-    //https://github.com/googlesamples/android-Camera2Basic
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_preview);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, CameraFragment.newInstance())
+                    .commit();
+        }
     }
+
 }
